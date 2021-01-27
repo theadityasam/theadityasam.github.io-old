ab9a7e4c25eb3495b106a7fec6b3f578b24afb3d
Aditya Samantaray <aditya.samantaray1@gmail.com>
2021-01-27 20:18:27 +0530

    Fix 404 img

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,14 +9,23 @@ const NotFoundPage: React.FC = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
-    <img
-      className="image"
-      src={image}
-      alt="404 Error"
-      width="500"
-      height="600"
-    />
     <p>You just hit a route that doesn&#39;t exist.</p>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "45vh",
+      }}
+    >
+      <img
+        className="image"
+        src={image}
+        alt="404 Error"
+        width="400"
+        height="300"
+      />
+    </div>
   </Layout>
 );
 

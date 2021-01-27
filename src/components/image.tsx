a16776f92bf99e81ee9a1991fd978dc052c7e4fc
Aditya Samantaray <aditya.samantaray1@gmail.com>
2021-01-27 17:46:28 +0530

    Add image component

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,34 +1,40 @@
-// import React from 'react'
+import React from "react";
 
-// interface Props {
-//     mode?: string;
-//     src: boolean;
-//     height?: number;
-//     width?: number;
+interface Props {
+  mode?: string;
+  src: boolean;
+  height?: number;
+  width?: number;
+}
 
-// }
+const Image: React.FC<Props> = ({
+  mode,
+  src,
+  height,
+  width,
+  style,
+  ...props
+}) => {
+  let modes = {
+    fill: "cover",
+    fit: "contain",
+  };
+  let size = modes[mode] || "contain";
 
-// const Image: React.FC<Props> = ({ mode, src, height, width, style, ...props }) => {
-//   let modes = {
-//     fill: "cover",
-//     fit: "contain",
-//   };
-//   let size = modes[mode] || "contain";
+  let defaults = {
+    height: height || 100,
+    width: width || 100,
+    backgroundColor: "gray",
+  };
 
-//   let defaults = {
-//     height: height || 100,
-//     width: width || 100,
-//     backgroundColor: "gray",
-//   };
+  let important = {
+    backgroundImage: `url("${src}")`,
+    backgroundSize: size,
+    backgroundPosition: "center center",
+    backgroundRepeat: "no-repeat",
+  };
 
-//   let important = {
-//     backgroundImage: `url("${src}")`,
-//     backgroundSize: size,
-//     backgroundPosition: "center center",
-//     backgroundRepeat: "no-repeat",
-//   };
+  return <div {...props} style={{ ...defaults, ...style, ...important }} />;
+};
 
-//   return <div {...props} style={{ ...defaults, ...style, ...important }} />;
-// };
-
-// export default Image;
+export default Image;

1eef6c234d55d8fb4890fa76bc223b11636aa30d
Aditya Samantaray <aditya.samantaray1@gmail.com>
2021-05-23 03:49:22 +0530

    Use env

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import Layout from "../components/layouts/resume-layout";
 import "../styles/resume.scss";
 
+const PDF =
+  "https://drive.google.com/file/d/" +
+  process.env.DRIVE_FILE +
+  "/preview?usp=embed_googleplus";
+
 // const PDF = process.env.GATSBY_RESUME;
 // const styles = {
 //   border: 0,
@@ -26,7 +31,7 @@ const Resume: React.FC = () => (
     </div> */}
     <div style={{ height: "100vh" }}>
       <iframe
-        src="https://drive.google.com/file/d/17wLkrnymU_S_o45mrxDHwSTziyKvDnrR/preview?usp=embed_googleplus"
+        src={PDF}
         style={{
           width: "100%",
           height: "100vh",
